3e6aa36db25ffa91a9ea6e7b2a9708823a991354
vrouker <vrouker@gmail.com>
2025-04-22 21:59:22 -0400

    started working on the new player form (not yet complete)

--- src/Components/NewPlayerForm.jsx
+++ src/Components/NewPlayerForm.jsx
@@ -1,11 +1,67 @@
-
+import { useState } from "react"
 
 
 function NewPlayerForm() {
-
+    const [name, setName] = useState("")
+    const [breed, setBreed] = useState("")
+    const [imageUrl, setImage] = useState("")
+    const [status, setStatus] = useState("")
     return (
         <>
-        
+        <h2>Add A New Puppy!</h2>
+        {
+            <form>
+                <label>
+                    Puppy Name:
+                    <input
+                        className="inputBox"
+                        name="name"
+                        required
+                        onChange={(e)=>setName(e.target.value)}
+                        value={name}
+                    />
+                </label>
+                <br/>
+                <label>
+                    Puppy Breed:
+                    <input 
+                        className="inputBox"
+                        name = "breed"
+                        required
+                        onChange={(e)=>setBreed(e.target.value)}
+                        value = {breed}
+                    />
+                </label>
+                <br/>
+                <label>
+                    Puppy Image:
+                    <input
+                        className="inputBox"
+                        name="image"
+                        oncChange={(e)=>setImage(e.target.value)}
+                        value = {imageUrl}
+                    />
+                </label>
+                <br/>
+                <label>
+                    Puppy Status:
+                    <input 
+                        className="inputBox"
+                        name="status"
+                        onChange={(e)=>setStatus(e.target.value)}
+                        value = {status}
+                    />
+                </label>
+                <br/>
+                <label>
+                    Team:
+                    <input/>
+                </label>
+                <br/>
+                <button>Sumbit!</button>
+            </form>
+
+        }
         
         </>
     )

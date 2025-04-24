a68aee83e2179fed3f2e6e91e19903b4c838d085
vrouker <vrouker@gmail.com>
2025-04-23 21:03:44 -0400

    delete button navigates to home page

--- src/Components/SinglePlayer.jsx
+++ src/Components/SinglePlayer.jsx
@@ -1,14 +1,16 @@
-
+import { useNavigate } from "react-router-dom";
 
 
 function SinglePlayer ({singlePlayer, setSinglePlayer}){
+    const navigate = useNavigate();
+
     async function handleDelete (id){
         try{
             const response = await fetch (`https://fsa-puppy-bowl.herokuapp.com/api/2501-ftb-web-et-pt/players/${id}`,{ method: "DELETE"});
         } catch (error){
             console.log(error)
         }
-
+        navigate("/")
     }
 
 
